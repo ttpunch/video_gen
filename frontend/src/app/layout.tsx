7eaa84d690035cbdd23e4b2e07d12815856efa3a
ttpunch <vinod418@gmail.com>
2026-06-14 08:33:49 +0530

    chore: update scheduler configuration settings and suppress hydration warning in layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${outfit.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
